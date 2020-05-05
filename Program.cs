2c19241101529015a66a497db04bb13d2017c7b3
Keegan-Young <59021335+Keegan-Young@users.noreply.github.com>
2020-05-05 10:45:46 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Chapter1WineGlassLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("\n\n\t\tXXXXXXXXXXXXX");
+            WriteLine("\t\t X         X");
+            WriteLine("\t\t  X       X");
+            WriteLine("\t\t   XXXXXXX");
+            WriteLine("\t\t      X");
+            WriteLine("\t\t      X");
+            WriteLine("\t\t    XXXXX");
+
+            ReadLine();
+        }
+    }
+}
